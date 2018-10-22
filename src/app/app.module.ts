eb2430658e87b1bdbe5120253b4dcdb821d36390
Winston Fernandes <winston23fernandes.wf@gmail.com>
2018-10-23 00:34:49 +0530

    Adding  Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {CustomersModule} from './customers/customers.module';
 import { SharedModule } from  './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,CustomersModule,SharedModule
+    BrowserModule,CustomersModule,SharedModule,CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent] //which component should load up first
